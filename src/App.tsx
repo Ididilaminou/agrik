39dd1d326e4102e87b244b6a63b970f67818335a
Laminou Souleymane <souleymanoulaminou38@gmail.com>
2025-05-27 05:26:51 +0100

    Mise à jour de pnpm-lock.yaml et package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import DashboardAgriK from './pages/DashboardAgriK';
 
-function App() {
+export default function App() {
   return <DashboardAgriK />;
 }
-
-export default App;
